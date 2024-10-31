18a8e26653197055e8ced92e4e5a47b843eb0b0f
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-10-31 22:07:13 +0100

    Créer les composants de base : Accueil, Services, Portfolio, Blog, Contact.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Cible les fichiers HTML et TypeScript dans le dossier src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
